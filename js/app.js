09ea78a4b311a7cf4791f5fd89ef39ba13f5389b
Anubis Lockward <anubis.lockward@gmail.com>
2024-06-04 06:53:43 -0400

    Load and show pokemon

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon
+const cont = document.querySelector('#container');
+const baseURL =
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon';
+
+for (let i = 0; i < 151; i++) {
+  const pokemon = document.createElement('div');
+  pokemon.classList.add('pokemon');
+  const label = document.createElement('span');
+  label.innerText = `#${i + 1}`;
+  const newImg = document.createElement('img');
+  newImg.src = `${baseURL}/${i + 1}.png`;
+
+  pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
+
+  cont.appendChild(pokemon);
+}
